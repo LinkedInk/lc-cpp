b11cba9a1610f9a34447dbd59e9d2701d512293b
Hendey Fan <hendeyfan98@gmail.com>
2025-10-12 15:36:41 +0800

    update headers.cpp

--- pasta/headers.cpp
+++ pasta/headers.cpp
@@ -7,3 +7,11 @@
 #include <algorithm>
 #include <unordered_map>
 using namespace std;
+
+#include "pasta/my_print.cpp"
+#include "pasta/binary_search.cpp"
+#include "pasta/tree_gen.cpp"
+
+#define loopI(i, s, e) for (size_t i = s; i < e; i++)
+
+// #include "pasta\headers.cpp"
