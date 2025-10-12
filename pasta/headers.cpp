d5083e57b0e66958cb8955000432acac83c3fa48
Hendey Fan <hendeyfan98@gmail.com>
2025-10-12 15:39:16 +0800

    update headers.cpp

--- pasta/headers.cpp
+++ pasta/headers.cpp
@@ -12,6 +12,9 @@ using namespace std;
 #include "pasta/binary_search.cpp"
 #include "pasta/tree_gen.cpp"
 
-#define loopI(i, s, e) for (size_t i = s; i < e; i++)
+#define loopst(i, s, e) for (size_t i = s; i < e; i++)
+#define loopstd(i, s, e) for (size_t i = s; i > e; i--)
+#define loopi(i, s, e) for (int i = s; i < e; i++)
+#define loopid(i, s, e) for (int i = s; i > e; i--)
 
 // #include "pasta\headers.cpp"
